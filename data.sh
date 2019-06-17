3f9f24ed196a58ace92beaea61601051934bd0da
qianlizeguo <1723877247@qq.com>
2019-06-17 23:34:30 +0800

    fix

--- data.sh
+++ data.sh
@@ -2,45 +2,45 @@
 
 echo ">>>> 开始配置文件夹"
 
-if [! -d "/home/docker" ]; then
+if [ ! -d "/home/docker" ]; then
     mkdir /home/docker
 fi
 
-if [! -d "/home/docker/mysql/data" ]; then
+if [ ! -d "/home/docker/mysql/data" ]; then
     mkdir /home/docker/mysql/data
 fi
 
-if [! -d "/home/docker/web" ]; then
+if [ ! -d "/home/docker/web" ]; then
     mkdir /home/docker/web
 fi
 
-if [! -d "/home/docker/php" ]; then
+if [ ! -d "/home/docker/php" ]; then
     mkdir /home/docker/php
 fi
 
-if [! -d "/home/docker/nginx" ]; then
+if [ ! -d "/home/docker/nginx" ]; then
     mkdir /home/docker/nginx
 fi
 
-if [! -d "/home/docker/nginx/conf.d" ]; then
+if [ ! -d "/home/docker/nginx/conf.d" ]; then
     mkdir /home/docker/nginx/conf.d
 fi
 
 echo ">>>> 开始配置文件是否存在"
 
-if [! -f "/home/docker/php/php.ini" ]; then
+if [ ! -f "/home/docker/php/php.ini" ]; then
     cp ./php/php.in /home/docker/php/
 fi
 
-if [! -f "/home/docker/php/www.conf"]; then
+if [ ! -f "/home/docker/php/www.conf"]; then
     cp ./php/www.conf /home/docker/php/
 fi
 
-if [! -f "/home/docker/nginx/nginx.conf"]; then
+if [ ! -f "/home/docker/nginx/nginx.conf"]; then
     cp ./nginx/nginx.conf /home/docker/nginx/
 fi
 
-if [! -f "/home/docker/nginx/conf.d/server.conf"]; then
+if [ ! -f "/home/docker/nginx/conf.d/server.conf"]; then
     cp ./ngin/server.conf /home/docker/nginx/conf.d/
 fi
 
